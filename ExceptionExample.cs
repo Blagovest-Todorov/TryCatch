7f44b7817d4482c54a5cacb4dbe74865763a38c8
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-23 11:13:38 +0200

    init

---
+++ ExceptionExample.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _02.ExceptionExample
+{
+    class ExceptionExample
+    {
+        public static void CauseFormatException()
+        {
+            string s = "an Invalid number";
+            Int32.Parse(s); //We try here to Parse String to number-this can not happen -> throws exception error
+        }
+        static void Main()
+        {
+            try
+            {
+                CauseFormatException(); //we call the method  here
+            }
+            catch (FormatException fe)
+            {
+
+                Console.Error.WriteLine("Exception: {0} \n{1}", fe.Message, fe.StackTrace); //we trhow error message
+            }
+        }
+    }
+}
